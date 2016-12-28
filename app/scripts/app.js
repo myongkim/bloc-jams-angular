44ee058762db8811fd1373336c6a3a02eabb26dd
Isaac <myong328@gmail.com>
2016-12-28 12:06:05 -0800

    assignment 3 completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,11 +8,16 @@
          $stateProvider
             .state('landing', {
                 url: '/',
+                //why does upper url does not have landing? where below album and collection we add album and collection?
                 templateUrl:'/templates/landiing.html'
          })
             .state('album', {
                 url: '/album',
                 templateUrl:'/templates/album.html'
+         })
+            .state('collection', {
+                url: '/collection',
+                templateUrl:'/templates/collection.html'
          });
      }
  
