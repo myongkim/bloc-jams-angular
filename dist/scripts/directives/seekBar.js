ef35543d43c076cbbae5307376a44ea10a3dc0c9
Isaac <myong328@gmail.com>
2017-01-19 15:48:13 -0800

    assignment-10-interfaces:revised

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -55,7 +55,7 @@
           scope.onClickSeekBar = function(event) {
               var percent = calculatePercent(seekBar, event);
               scope.value = percent * scope.max;
-              notify-OnChange(scope.value);
+              notifyOnChange(scope.value);
           };
 
           scope.trackThumb = function() {
@@ -63,7 +63,7 @@
                 var percent = calculatePercent(seekBar, event);
                 scope.$apply(function() {
                     scope.value = percent * scope.max;
-                    notify-OnChange(scope.value);
+                    notifyOnChange(scope.value);
                 });
             });
 
